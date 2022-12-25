a7b53bc8134f29de5445126ff2d83ef2f1ea7fec
Yana <yana229910nug@yandex.ru>
2022-12-25 23:29:16 +0300

    решение задачи

---
+++ test_task/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3.
+Console.WriteLine("Введите число элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите элементы массива:");
+string [] array = new string[n];
+
+for(int i=0;i<array.Length; i++)
+{
+  array[i] = Console.ReadLine();
+}
+
+Console.WriteLine($"[" + string.Join(", ", array) +"]"); 
+
+
+string [] array2 = new string[array.Length];
+
+for(int i = 0; i < array.Length; i++)
+{
+    int limit = 3;
+    if(array[i].Length <= limit)
+    {
+        array2[i]=array[i];
+    }    
+}
+
+Console.WriteLine(string.Join(" ", array2));
